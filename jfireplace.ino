5dde1dd83178b74d6a930b762b0832bb5206cc99
justinterada <jtvkws001@gmail.com>
2016-01-18 15:07:15 -0600

    Actually make it an on-off controlled by SmartThings

--- jfireplace.ino
+++ jfireplace.ino
@@ -1,32 +1,121 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
+//*****************************************************************************
+/// @file
+/// @brief
+///   Arduino SmartThings Shield LED Example 
+//*****************************************************************************
+#include <SoftwareSerial.h>   //TODO need to set due to some weird wire language linker, should we absorb this whole library into smartthings
+#include <SmartThings.h>
 
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
+#define PIN_THING_RX    3
+#define PIN_THING_TX    2
+
+
+SmartThingsCallout_t messageCallout;    // call out function forward decalaration
+SmartThings smartthing(PIN_THING_RX, PIN_THING_TX, messageCallout);  // constructor
+
+
+int switchPin = 5;
+bool isDebugEnabled;    // enable or disable debug in this example
+int stateLED;           // state to track last set value of LED
+
+void setup()
+{
+  // setup default state of global variables
+  isDebugEnabled = true;
+  stateLED = 0;                 // matches state of hardware pin set below
+  
+  // setup hardware pins 
+  pinMode(switchPin, OUTPUT);     // define PIN_LED as an output
+  digitalWrite(switchPin, LOW);   // set value to LOW (off) to match stateLED=0
+
+  if (isDebugEnabled)
+  { // setup debug serial port
+    Serial.begin(9600);         // setup serial with a baud rate of 9600
+    Serial.println("setup..");  // print out 'setup..' on start
+  }
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(6, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(5000);              // wait for a second
-  digitalWrite(6, HIGH);    // turn the LED off by making the voltage LOW
-  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
-  delay(5000);              // wait for a second
+
+void loop()
+{
+  // run smartthing logic
+  smartthing.run();
 }
+
+void on()
+{
+  stateLED = 1;                 // save state as 1 (on)
+  digitalWrite(switchPin, HIGH);  // turn switch on
+  smartthing.shieldSetLED(0, 0, 1);
+  smartthing.send("on");        // send message to cloud
+  Serial.println("on");
+}
+
+void off()
+{
+  stateLED = 0;                 // set state to 0 (off)
+  digitalWrite(switchPin, LOW);   // turn switch off
+  smartthing.shieldSetLED(0, 0, 0);
+  smartthing.send("off");       // send message to cloud
+  Serial.println("off");
+}
+
+void hello()
+{
+  Serial.println("brobasaur");
+  smartthing.send("colors!");
+  smartthing.shieldSetLED(1, 0, 0);
+  delay(200);
+  smartthing.shieldSetLED(0, 1, 0);
+  delay(200);
+  smartthing.shieldSetLED(0, 0, 1);
+  delay(200);
+  smartthing.shieldSetLED(1, 1, 0);
+  delay(200);
+  smartthing.shieldSetLED(1, 1, 1);
+  delay(200);
+  smartthing.shieldSetLED(1, 0, 1);
+  delay(200);
+  smartthing.shieldSetLED(0, 1, 1);
+  delay(200);
+  smartthing.shieldSetLED(3, 2, 1);
+  delay(200);
+  smartthing.shieldSetLED(1, 2, 3);
+  delay(200);
+  smartthing.shieldSetLED(2, 2, 4);
+  delay(200);
+  smartthing.shieldSetLED(4, 3, 1);
+     delay(200);
+  smartthing.shieldSetLED(0, 0, 0);
+  smartthing.send("fancy");
+  
+}
+
+
+void messageCallout(String message)
+{
+  // if debug is enabled print out the received message
+  if (isDebugEnabled)
+  {
+    Serial.print("Received message: '");
+    Serial.print(message);
+    Serial.println("' ");
+  }
+
+  // if message contents equals to 'on' then call on() function
+  // else if message contents equals to 'off' then call off() function
+  if (message.equals("on"))
+  {
+    on();
+  }
+  else if (message.equals("off"))
+  {
+    off();
+  }
+  else if (message.equals("hello"))
+  {
+    hello();
+  }
+}
+
